239135395eb9b56b76be775a043faa1cf37459d7
Limanskyi1 <125376956+Limanskyi1@users.noreply.github.com>
2025-06-18 12:19:40 +0300

    create welcome page , customize styles and shadcn

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,8 +36,10 @@
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <!-- <link rel="preconnect" href="https://fonts.bunny.net"> -->
+        <!-- <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" /> -->
+        <link rel="preconnect" href="https://fonts.cdnfonts.com/css/sf-pro-display" />
+        <link href="https://fonts.cdnfonts.com/css/sf-pro-display" rel="stylesheet">
 
         @routes
         @viteReactRefresh
