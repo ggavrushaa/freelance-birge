3b8bc1783485ac605a1e0115429bccdb3b8ffca9
Limanskyi1 <125376956+Limanskyi1@users.noreply.github.com>
2025-07-31 19:01:22 +0300

    fixes use wep app

--- resources/js/shared/hooks/use-web-app.ts
+++ resources/js/shared/hooks/use-web-app.ts
@@ -2,8 +2,10 @@
 
 export const useWebApp = () => {
   const data = window.Telegram.WebApp.initDataUnsafe;
-  console.log("useWebApp data", data);
-  const user = data.user || {
+  const user = data.user ? {
+    telegram_id: data.user.id,
+    username: data.user.username,
+  } : {
     telegram_id: "123",
     username: "Bohdan",
   };
