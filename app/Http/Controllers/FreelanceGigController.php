9dc4bea5f89ae8ab8af9526bd9159a9ae5ae3c86
ggavrushaa <gavrilnikitin2020@gmail.com>
2025-08-15 15:54:27 +0300

    fix

--- app/Http/Controllers/FreelanceGigController.php
+++ app/Http/Controllers/FreelanceGigController.php
@@ -53,7 +53,7 @@ class FreelanceGigController extends Controller
 
     public function show($id)
     {
-        $gig = FreelanceGig::with('tariffs', 'author')->findOrFail($id);
+        $gig = FreelanceGig::with('tariffs', 'freelancer')->findOrFail($id);
 
         return Inertia::render('freelance/gig/show.page', [
             'gig' => $gig,
