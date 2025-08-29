cbbe325e5fcc6085e5369de9e12fa2a533c399f2
Limanskyi1 <125376956+Limanskyi1@users.noreply.github.com>
2025-08-29 13:03:34 +0300

    create link in archive page

--- resources/js/pages/order/archive.page.tsx
+++ resources/js/pages/order/archive.page.tsx
@@ -1,7 +1,9 @@
 import { Job } from '@/entities/job';
 import { OrderArchiveCard, OrderCard, useGetOrders } from '@/entities/order';
+import { ROUTES } from '@/shared/config/routes';
 import { statusIcons } from '@/shared/consts';
 import { LayoutWithNavbar } from '@/shared/layouts/layout-with-navbar';
+import { Link } from '@inertiajs/react';
 import { ReactNode } from 'react';
 
 export const OrderArchivePage = () => {
@@ -9,17 +11,24 @@ export const OrderArchivePage = () => {
     const archive = (data?.archive ?? []) as Job[];
     return (
         <section className="flex-1 flex-col bg-[#efeff4] p-6 pt-25">
-            <OrderArchiveCard count={archive.length} className='mb-4'/>
+            <OrderArchiveCard count={archive.length} className="mb-4" />
             <div className="flex flex-col gap-2">
                 {archive.map((archiveItem) => (
-                    <OrderCard
-                        icon={<img src={statusIcons[archiveItem.status]} className="w-7 opacity-20" />}
-                        title={archiveItem.name}
-                        status={archiveItem.status}
-                        terms={archiveItem.terms}
-                        price={parseFloat(archiveItem.price)}
-                        count={null}
-                    />
+                    <Link key={archiveItem.id} href={ROUTES.orderShow(archiveItem.id)}>
+                        <OrderCard
+                            icon={
+                                <img
+                                    src={statusIcons[archiveItem.status]}
+                                    className="w-7 opacity-20"
+                                />
+                            }
+                            title={archiveItem.name}
+                            status={archiveItem.status}
+                            terms={archiveItem.terms}
+                            price={parseFloat(archiveItem.price)}
+                            count={null}
+                        />
+                    </Link>
                 ))}
             </div>
         </section>
